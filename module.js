ed98a57b0de98aa2f73f35d2329d1a3cfa633fef
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2024-09-08 18:54:02 +0530

    Add files via upload

--- module.js
+++ module.js
@@ -213,6 +213,7 @@ document.addEventListener('click',(e)=>{
                 var div=sndC('div');
                 if(obj.p1===email){div.setAttribute('class','p1')}
                 else{div.setAttribute('class','p2')}
+                div.setAttribute('id',obj.chatId);
                 div.innerHTML=`<pre>${obj.chat}</pre>`;
                 snd('.chats').appendChild(div);
             });
@@ -228,7 +229,8 @@ document.addEventListener('click',(e)=>{
                 set(ref(db,`/chats/${Date.now()}`),{
                     p1:email,
                     p2:email2,
-                    chat:snd('#msg').value
+                    chat:snd('#msg').value,
+                    chatId:Date.now()
                 });
                 var div=sndC('div');
                 div.innerHTML=`<pre>${snd('#msg').value.replaceAll("<","&lt;").replaceAll(">","&gt;")}</pre>`;
@@ -245,28 +247,26 @@ document.addEventListener('click',(e)=>{
 
 function checkMsg(){
     recheck=setInterval(()=>{
-
-       var chatCount=document.querySelectorAll('.p2');
-
-        get(ref(db,'/chats')).then((data)=>{
-
-            var dataVal=data.val();
-            var p2Count=[];
-            for(var x in dataVal){
-                if(dataVal[x].p2===email && dataVal[x].p1===email2){
-                   p2Count.push(dataVal[x].chat);
-                   if(p2Count.length > chatCount.length){                   
-                    var div=sndC('div');
-                    div.setAttribute('class','p2');
-                    div.innerHTML=`<pre>${p2Count[p2Count.length-1]}</pre>`;
-                    snd('.chats').appendChild(div);
-                    received.play();
-                    snd('.chats').scrollTop = snd('.chats').scrollHeight;
-                   }
-                }
-            }
-
-        });
-
-    },1000);
+        var p2Array=document.querySelectorAll('.p2');
+        var msgId=p2Array[p2Array.length-1].id;
+       var msgArray=[];
+       get(ref(db,'/chats')).then((data)=>{
+       var dataVal=data.val();
+       for(var x in dataVal){
+        if(dataVal[x].p2===email && dataVal[x].p1===email2){
+            msgArray.push(dataVal[x]);
+        }
+       }
+       }).then(()=>{
+        if(msgArray[msgArray.length-1].chatId != msgId){
+            var div=sndC('div');
+            div.setAttribute('class','p2');
+            div.setAttribute('id',msgArray[msgArray.length-1].chatId);
+            div.innerHTML=`<pre>${msgArray[msgArray.length-1].chat}</pre>`;
+            snd('.chats').appendChild(div);
+            received.play();
+            snd('.chats').scrollTop = snd('.chats').scrollHeight;
+        }
+       })
+    },500);
 }
